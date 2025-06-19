f824ee965ab3e435781289d57974fb2d683dbce3
HP-006 <hariadit2006@gmail.com>
2025-06-19 14:40:43 +0530

    objs

---
+++ objects.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    console.log(a+b);
+}
+
+
+let newObject = {
+    name: "John Doe",
+    age:25,
+    func: (a, b) => {
+        console.log(a,b)
+    },
+};
+console.log(newObject.name);
+console.log(newObject.age);
+newObject.func(2,3);
