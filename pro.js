5272b97d4a3ee9d45463c26801eae396d9fb0026
HP-006 <hariadit2006@gmail.com>
2025-06-19 12:22:00 +0530

    learned

---
+++ pro.js
@@ -0,0 +1,10 @@
+let d=["39","88","27","18","15","11","56"];
+let n=[];
+for(let i=0;i<d.length;i++){ 
+if(d[i]>=18){    
+    console.log( d[i] + " can vote");  
+    n.push(d[i]);
+
+}
+}
+console.log(n);
