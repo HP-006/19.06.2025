506922f446b5a50dfc38ed8168c632cd4b73511c
HP-006 <hariadit2006@gmail.com>
2025-06-19 15:25:32 +0530

    complete  reduced method

--- functions.js
+++ functions.js
@@ -28,3 +28,33 @@ const arrowFuncVar =  () => {
     console.log("Arrow fnx");
 };
 arrowFuncVar();
+
+//Filter,Map,Reduce
+let nums = [1,2,3,4,5];
+let evens = nums.filter((n) => {
+    return (n%2 === 0);
+} );
+
+console.log(evens);
+
+
+let newList = [];
+
+for(let i = 0;i < nums.length; i++){
+    if(nums[i]%2===0) {
+        newList.push (nums[i]);
+    }
+}
+console.log(newList); 
+console.log("Using Map method");
+let squares = nums.map((n) =>{
+    return n*n;
+});
+
+console.log(squares);
+console.log("Using reduce mathod");
+//let sum = nums.reduce((sum,n)=> sum+n,0);
+let newSum = nums.reduce((Sum,n) =>{
+    return sum +n;
+},(sum = 0));
+console.log(newSum);
